de80d93923d5a2a2279f8774a128131176130a97
mansi <mnayak@bestpeers.com>
2024-07-11 12:21:32 +0530

    pw

---
+++ randomprac4.py
@@ -0,0 +1,11 @@
+import random
+import string 
+
+def random_pw(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(characters) for i in range(length))
+
+print(random_pw(14))
+
+#a function that generates a random password of a given length. The password should include uppercase, lowercase, digits, and special characters.
+
