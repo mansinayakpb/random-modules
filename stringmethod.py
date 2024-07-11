04a35a6238713b1fc06c7429b50912ceb552d82f
mansi <mnayak@bestpeers.com>
2024-07-11 18:38:16 +0530

    string method

---
+++ stringmethod.py
@@ -0,0 +1,95 @@
+#capitalize()
+my_str = "hello world"
+print(my_str.capitalize())  
+
+#lower()
+my_str = "Hello World"
+print(my_str.lower())  
+
+#upper()
+my_str = "hello world"
+print(my_str.upper())  
+
+#title()
+my_str = "hello world"
+print(my_str.title())  
+
+#swapcase()
+my_str = "Hello World"
+print(my_str.swapcase())  
+
+#strip()
+my_str = "  hello world  "
+print(my_str.strip()) 
+
+#lstrip()
+my_str = "  hello world"
+print(my_str.lstrip()) 
+
+#rstrip()
+my_str = "hello world  "
+print(my_str.rstrip()) 
+
+#find()
+my_str = "hello world"
+print(my_str.find("world"))  
+
+#rfind()
+my_str = "hello world world"
+print(my_str.rfind("world"))  
+
+#replace()
+my_str = "hello world"
+print(my_str.replace("world", "there"))  
+
+#split()
+my_str = "hello world"
+print(my_str.split())  
+
+#join()
+my_list = ["hello", "world"]
+print(" ".join(my_list)) 
+
+#startswith()
+my_str = "hello world"
+print(my_str.startswith("hello")) 
+
+#endswith()
+my_str = "hello world"
+print(my_str.endswith("world"))  
+
+#count()
+my_str = "hello world world"
+print(my_str.count("world"))  
+
+#isalpha()
+my_str = "hello"
+print(my_str.isalpha())  
+
+#isdigit()
+my_str = "12345"
+print(my_str.isdigit())  
+
+#isalnum()
+my_str = "hello123"
+print(my_str.isalnum())  
+
+#isspace()
+my_str = "   "
+print(my_str.isspace()) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
