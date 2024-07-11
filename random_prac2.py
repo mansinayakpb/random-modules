9368ea1cbf250e5003dc0390293433a8b5a901ce
mansi <mnayak@bestpeers.com>
2024-07-11 11:54:49 +0530

    element

---
+++ random_prac2.py
@@ -0,0 +1,9 @@
+import random
+
+def random_ele(my_list):
+    return random.choice(my_list)
+
+my_list = [10, 20, 30, 50, 40, 80, 60]
+print(random_ele(my_list))
+
+#random select element from list
