1f12932ae00e7a50ba5b611f80c4c14d921c74c1
mansi <mnayak@bestpeers.com>
2024-07-12 17:44:53 +0530

    abstraction

---
+++ abstraction.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+
+def print_shape_details(shape):
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
+
+rect = Rectangle(5, 3)
+circle = Circle(4)
+
+print_shape_details(rect)
+print_shape_details(circle)
